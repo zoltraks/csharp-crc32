75777816a1bbbd68d693072d4ea254f07e39020d
Filip Golewski <f.golewski@gmail.com>
2024-09-12 03:20:06 +0200

    Source

---
+++ src/Crc32/Crc32.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+
+namespace Crc32
+{
+    public class Crc32 : HashAlgorithm
+    {
+        private uint crc;
+
+        private readonly uint[] table;
+
+        public override void Initialize()
+        {
+            crc = 0xffffffff;
+        }
+
+        protected override void HashCore(byte[] buffer, int start, int length)
+        {
+            for (int i = start; i < length; i++)
+            {
+                byte next = buffer[i];
+                byte index = (byte)((crc & 0xff) ^ next);
+                crc = (crc >> 8) ^ table[index];
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            crc = ~crc;
+            return BitConverter.GetBytes(crc);
+        }
+
+        public Crc32() : this(0xedb88320) { }
+
+        public Crc32(uint polynomial)
+        {
+            HashSizeValue = 32;
+
+            Initialize();
+
+            table = new uint[256];
+
+            for (uint i = 0; i < 256; i++)
+            {
+                uint crc = i;
+                for (uint j = 0; j < 8; j++)
+                {
+                    if ((crc & 1) == 1)
+                    {
+                        crc = (crc >> 1) ^ polynomial;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+                table[i] = crc;
+            }
+        }
+    }
+}
---
+++ src/Crc32/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Crc32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string friendlyName = AppDomain.CurrentDomain.FriendlyName;
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine($"Usage: {friendlyName} <filename>");
+                return;
+            }
+
+            string filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File not found: " + filePath);
+                return;
+            }
+
+            try
+            {
+                using FileStream fs = File.OpenRead(filePath);
+                using var crc32 = new Crc32();
+                byte[] hash = crc32.ComputeHash(fs);
+                uint result = BitConverter.ToUInt32(hash, 0);
+                Console.WriteLine(result.ToString("X8"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
