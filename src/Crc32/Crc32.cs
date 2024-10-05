524f6d0e9864411ae47269af9b612de20fd01b07
Filip Golewski <f.golewski@gmail.com>
2024-10-05 18:29:43 +0200

    Refactoring

--- src/Crc32/Crc32.cs
+++ src/Crc32/Crc32.cs
@@ -4,30 +4,9 @@ namespace Crc32
 {
     public class Crc32 : HashAlgorithm
     {
-        private uint crc;
+        private uint checksum;
 
         private readonly uint[] table;
-
-        public override void Initialize()
-        {
-            crc = 0xffffffff;
-        }
-
-        protected override void HashCore(byte[] buffer, int start, int length)
-        {
-            for (int i = start; i < length; i++)
-            {
-                byte next = buffer[i];
-                byte index = (byte)((crc & 0xff) ^ next);
-                crc = (crc >> 8) ^ table[index];
-            }
-        }
-
-        protected override byte[] HashFinal()
-        {
-            crc = ~crc;
-            return BitConverter.GetBytes(crc);
-        }
 
         public Crc32() : this(0xedb88320) { }
 
@@ -41,20 +20,41 @@ namespace Crc32
 
             for (uint i = 0; i < 256; i++)
             {
-                uint crc = i;
+                uint next = i;
                 for (uint j = 0; j < 8; j++)
                 {
-                    if ((crc & 1) == 1)
+                    if ((next & 1) == 1)
                     {
-                        crc = (crc >> 1) ^ polynomial;
+                        next = (next >> 1) ^ polynomial;
                     }
                     else
                     {
-                        crc >>= 1;
+                        next >>= 1;
                     }
                 }
-                table[i] = crc;
+                table[i] = next;
             }
+        }
+
+        public override void Initialize()
+        {
+            checksum = 0xffffffff;
+        }
+
+        protected override void HashCore(byte[] buffer, int start, int length)
+        {
+            for (int i = start; i < length; i++)
+            {
+                byte next = buffer[i];
+                byte index = (byte)((checksum & 0xff) ^ next);
+                checksum = (checksum >> 8) ^ table[index];
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            checksum = ~checksum;
+            return BitConverter.GetBytes(checksum);
         }
     }
 }
